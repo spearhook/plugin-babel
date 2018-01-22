73c789754b2a058d840f2939aa9d6f40151ece7b
Mike Botsko <botsko@gmail.com>
2018-01-22 07:20:39 -0800

    Adds error forwarding

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const through2 = require('through2');
 
 module.exports = function(opts) {
     return () => {
-        return through2.obj((file, encoding, cb) => {
+        return through2.obj(function(file, encoding, cb) {
             if (file.isNull()) {
                 // nothing to do
                 return cb(null, file);
             }
 
-            const result = babel.transform(file.contents.toString(), opts);
+            try {
+                const result = babel.transform(file.contents.toString(), opts);
 
-            file.contents = new Buffer(result.code);
+                file.contents = new Buffer(result.code);
+            } catch (err) {
+                this.emit('spearhook:error', { err, file });
+            }
 
             cb(null, file);
         });
