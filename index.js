30159423b29e898b04366b6e27503df4dca7e49f
Mike Botsko <botsko@gmail.com>
2019-02-14 13:30:01 -0800

    Fixes syntax error, upgrades dependencies

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const babel = require('@babel/core');
 const through2 = require('through2');
 
 module.exports = function(opts) {
-    return () => {
-        return through2.obj(function(file, encoding, cb) => {
-            if (file.isNull()) {
-                // nothing to do
-                return cb(null, file);
-            }
+    return () => through2.obj(function(file, encoding, cb) {
+        if (file.isNull()) {
+            // nothing to do
+            return cb(null, file);
+        }
 
-            try {
-                const result = babel.transform(file.contents.toString(), opts);
+        try {
+            const result = babel.transform(file.contents.toString(), opts);
 
-                file.contents = Buffer.from(result.code);
-            } catch (err) {
-                this.emit('spearhook:error', { err, file });
-            }
+            file.contents = Buffer.from(result.code);
+        } catch (err) {
+            this.emit('spearhook:error', { err, file });
+        }
 
-            cb(null, file);
-        });
-    }
+        cb(null, file);
+    });
 };
